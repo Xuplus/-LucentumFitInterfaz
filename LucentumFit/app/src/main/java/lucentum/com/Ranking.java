1cc1de0d2022fe4cac94309f7add2b44550226a9
Leo Rodriguez Martinez <LeoRodMrez@MacBook-Pro-de-Leonardo.local>
2016-05-10 16:00:23 +0200

    ranking cambiado listview

--- LucentumFit/app/src/main/java/lucentum/com/Ranking.java
+++ LucentumFit/app/src/main/java/lucentum/com/Ranking.java
@@ -2,12 +2,22 @@ package lucentum.com;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Ranking extends AppCompatActivity {
+
+    private ListView list;
+    private String[] sistemas= {"juan","carla","pedro", "paco","ana","carlos","esteban","jose","sara","chema","sandra", "carol", "cesar", "fernando"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ranking);
+
+        list = (ListView) findViewById(R.id.user_list);
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, sistemas);
+        list.setAdapter(adaptador);
+
     }
 }
