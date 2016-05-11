a58449e4ca6a26b78d6a183fb92a45d68caa7b66
Xuplus <antovillaescusa@gmail.com>
2016-05-11 16:27:58 +0200

    PArece que funciona todo, queda ajustar un poco los aspectos visuales, los bordes de los botones se quedan un poco raros

--- LucentumFit/app/src/main/java/lucentum/com/Dashboard.java
+++ LucentumFit/app/src/main/java/lucentum/com/Dashboard.java
@@ -19,7 +19,7 @@ public class Dashboard extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        setContentView(R.layout.fragment_dashboard);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
