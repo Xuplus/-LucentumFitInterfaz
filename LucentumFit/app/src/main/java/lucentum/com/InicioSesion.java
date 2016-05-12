d1f7d1e1c6bdc031b56afa710953c93e7edb7f02
navarrillo <sergionm94@gmail.com>
2016-05-12 16:09:29 +0200

    Perfil, inicio sesion, registro
    Hechas las activytis perfil, inicio sesion y registro a falta de
    conectar con la API

--- LucentumFit/app/src/main/java/lucentum/com/InicioSesion.java
+++ LucentumFit/app/src/main/java/lucentum/com/InicioSesion.java
@@ -5,8 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -14,10 +17,14 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class InicioSesion extends AppCompatActivity {
 
     RequestQueue requestQueue;
-    String loginURL = "http://alacantfit.herokuapp.com/usuarios";
+    String loginURL = "http://alacantfit.herokuapp.com/login/";
+    EditText usuario,pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +34,75 @@ public class InicioSesion extends AppCompatActivity {
         setContentView(R.layout.activity_inicio_sesion);
         //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
+        usuario = (EditText) findViewById(R.id.et_usuario);
+        pass = (EditText) findViewById(R.id.et_password);
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
 
     }
+
+    public void IniciarSesion(View v) {
+        cambioActivity();
+    }
+   /* public void IniciarSesion(View v)
+    {
+
+            StringRequest request = new StringRequest(Request.Method.POST, loginURL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+
+                    MostrarToast("Bienvenido "+usuario.getText().toString());
+                    cambioActivity();
+                }
+            }, new Response.ErrorListener(){
+
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    System.out.println("RESPONSE NO");
+                    MostrarToast("Credenciales indorrectas.");
+
+                }
+            }) {
+
+
+                @Override
+                protected Map<String,String> getParams() throws
+                        AuthFailureError {
+                    Map<String, String> parametros = new HashMap<String, String>();
+                    parametros.put("Content-Type", "application/json; charset=utf-8");//IMPORTANTÍSIMA //por ésta linea me daba error 404
+                    //parametros.put("Nombre", "Admin");
+                    parametros.put("Usuario", usuario.getText().toString());
+                    parametros.put("Pass", pass.getText().toString());
+                    //parametros.put("Correo", email.getText().toString());
+                    // parametros.put("Ciudad", "Alicante");
+                    // parametros.put("Pais", "España");
+                    // parametros.put("Edad", "22");
+                    // parametros.put("Altura", "174");
+                    // parametros.put("Peso", "70");
+                    // parametros.put("Imagen", "null");
+                    return parametros;
+                    //return toJSON();
+                }
+            };
+            requestQueue.add(request);
+    }*/
+
 
     public void Registro(View v)
     {
         Intent intent = new Intent(this,Registro.class);
         startActivity(intent);
+    }
+
+    public void MostrarToast(String mensaje)
+    {
+        Toast toast = Toast.makeText(this, mensaje, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void cambioActivity()
+    {
+        Intent intent = new Intent(this,Perfil.class);
+        startActivity(intent);
+        finish();
     }
 }
