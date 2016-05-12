03c55e12f1c7952373373178fcf4c499b54922f8
Francisco Maciá <Francisco Maci>
2016-05-12 16:28:12 +0200

    Añadido interfaz contactos

--- LucentumFit/app/src/main/java/lucentum/com/ListaContactos.java
+++ LucentumFit/app/src/main/java/lucentum/com/ListaContactos.java
@@ -1,15 +1,26 @@
 package lucentum.com;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+
+import java.util.List;
 
 /**
  * Created by Fran on 05/05/2016.
  */
-public class ListaContactos extends ArrayAdapter<String>{
-    public ListaContactos(Context context, int resource) {
-        super(context, resource);
+class ListaContactos extends ArrayAdapter<String>{
+    public ListaContactos(Context context, List<String> datos) {
+        super(context, R.layout.activity_lista_contactos, datos);
     }
-    
 
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //return super.getView(position, convertView, parent);
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View CustoView = inflater.inflate(R.layout.activity_lista_contactos, parent, false);
+
+    }
 }
