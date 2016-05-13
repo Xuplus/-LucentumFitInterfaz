9ee69cfd5a84894e51374e1c83ce3beb3e18eb94
JOAQUIN VASALO <ximo.vasalo@gmail.com>
2016-05-13 11:16:23 +0200

    añado codig de rutas
    ratadetallada y la creacion de ruta junto con el permiso de gradle de
    google para los mapas y ademas de la compilacion de una libreria
    necesaria para los mapas

--- LucentumFit/app/src/main/java/lucentum/com/Contactos.java
+++ LucentumFit/app/src/main/java/lucentum/com/Contactos.java
@@ -8,6 +8,6 @@ public class Contactos extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contactos);
+       // setContentView(R.layout.activity_contactos);
     }
 }
