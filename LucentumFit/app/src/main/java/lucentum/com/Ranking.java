080099dc9bc9816f5b8b9b81b85272f7cc8d7f10
Leo Rodriguez Martinez <LeoRodMrez@MacBook-Pro-de-Leonardo.local>
2016-05-17 18:01:09 +0200

    restauración rama + ranking java actualizado

--- LucentumFit/app/src/main/java/lucentum/com/Ranking.java
+++ LucentumFit/app/src/main/java/lucentum/com/Ranking.java
@@ -1,31 +1,47 @@
 package lucentum.com;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.view.View;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Ranking extends AppCompatActivity {
 
     private ListView list;
-    private String[] amigos = {"juan", "carla", "pedro", "paco", "ana", "carlos", "esteban", "jose", "sara", "chema", "sandra", "carol", "cesar", "fernando"};
-    private String[] global = {"bolt", "jornet", "stevenson", "sergio", "smith", "charlie", "martinez", "jose luis", "amaya", "petrov", "champion", "april", "leslie", "ron"};
-
-
-    //TODO:
-        //  MÉTODO PARA LEER DE JSON O LO QUE SEA Y GENERAR EL STRING
-
-    //TODO:
-        //  MÉTODO PARA AÑADIR LA NUMERACIÓN
-
+    RequestQueue requestQueue;
+    String rankGlobal = "http://46.101.84.36/ranking";
+    String rankAmigos ="http://46.101.84.36/ranking/"; //+id
+    String rankPersonal ="http://46.101.84.36/ranking/Personal/"; //+id
+    String rankPersonalPos = "http://46.101.84.36/ranking/Posicion/"; //+id
+    private String[] amigos = {};
+    private String[] global = {};
+    private String[] personal = {};
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ranking);
+
+        SharedPreferences preferences = getSharedPreferences("usuario", Context.MODE_PRIVATE);
+        String usuario = preferences.getString("usu", "null");
+
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+        cargarRankingAmigos(usuario);
+        cargarRankingGlobal();
+        cargarRankingPersonal(usuario);
 
         //SE MUESTRA POR DEFECTO EL RANKING DE AMIGOS
         list = (ListView)
@@ -59,9 +75,110 @@ public class Ranking extends AppCompatActivity {
 
                 ArrayAdapter<String> adaptador = new ArrayAdapter<String>(Ranking.this, android.R.layout.simple_list_item_1, global);
                 list.setAdapter(adaptador);
+
+            }
+
+        });
+
+        // BOTON PARA ELEGIR EL RANKING PERSONAL
+        final Button button_personal = (Button) findViewById(R.id.btn_ranking_personal);
+        button_personal.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                list = (ListView)
+
+                        findViewById(R.id.user_list);
+
+                ArrayAdapter<String> adaptador = new ArrayAdapter<String>(Ranking.this, android.R.layout.simple_list_item_1, personal);
+                list.setAdapter(adaptador);
+
             }
 
         });
 
     }
+
+    public void cargarRankingAmigos(String usuario) {
+        StringRequest request = new StringRequest(Request.Method.GET, rankAmigos+usuario, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                JSONObject jObject  = null; // json
+                try {
+                    jObject = new JSONObject(response);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    String parametro = jObject.getString("Nombre");
+
+                    System.out.println("RESPONSE "+parametro);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        }) {};
+        requestQueue.add(request);
+    }
+
+    public void cargarRankingGlobal() {
+        StringRequest request = new StringRequest(Request.Method.GET, rankGlobal, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                JSONObject jObject  = null; // json
+                try {
+                    jObject = new JSONObject(response);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    String parametro = jObject.getString("Nombre");
+
+                    System.out.println("RESPONSE "+parametro);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        }) {};
+        requestQueue.add(request);
+    }
+
+    public void cargarRankingPersonal(String usuario) {
+        StringRequest request = new StringRequest(Request.Method.GET, rankPersonal+usuario, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                JSONObject jObject  = null; // json
+                try {
+                    jObject = new JSONObject(response);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    String parametro = jObject.getString("Nombre");
+
+                    System.out.println("RESPONSE "+parametro);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        }) {};
+        requestQueue.add(request);
+    }
 }
+
